70c4265bc105224662d6adb0ec31592473e4bfdb
Srikanth-Munusamy <srikanth28092004@gmail.com>
2024-12-19 12:01:11 +0530

    Docker edit

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "srikanth",
-  password: "Srikanth@2004",
-  database: "employeeforms",
-});
-
 // const pool = mysql.createPool({
-//   host: "db",
-//   user: "root",
-//   password: "mysql123",
-//   database: "employeesform",
+//   host: "localhost",
+//   user: "srikanth",
+//   password: "Srikanth@2004",
+//   database: "employeeforms",
 // });
+
+const pool = mysql.createPool({
+  host: "db",
+  user: "root",
+  password: "mysql123",
+  database: "employeesform",
+  port: 3306,
+});
 
 const promisePool = pool.promise();
 
